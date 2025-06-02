055f19ebbb370b2901fb78e6b6f65787034754f9
Sheron <sherony060117@gmail.com>
2025-06-02 15:37:54 -0400

    File location changes

--- fall2023/script.js
+++
@@ -1,8 +0,0 @@
-// faq -----------------------------------------------------------------------------------------
-$(".question").each(function() {
-    $(this).click(function(){
-        $(this).find("p").toggle();
-        $(this).toggleClass("faq_exp");
-        //$(this).css('listStyleImage', $(this).hasClass("selected")?sel:unsel);    
-    })
-});
